df79f8596eb4b9183b6af0b58a3fae351eb20468
Dean B <deantburrows88@gmail.com>
2025-12-30 18:26:21 +0100

    improved version

--- Program.cs
+++ Program.cs
@@ -17,31 +17,35 @@ Console.WriteLine("[M]ultiply numbers");
 
 string userChoice = Console.ReadLine();
 
-string op = "";
-int result = 0;
+if (userChoice != null)
+{
+    string op = "";
+    int result = 0;
 
-switch (userChoice)
-{
-    case "a":
-        op = " + ";
-        result = firstNumber + secondNumber;
-        break;
-    case "s":
-        op = " - ";
-        result = firstNumber - secondNumber;
-        break; 
-    case "m":
-        op = " - ";
-        result = firstNumber - secondNumber;
-        break;
-}
+    switch (userChoice.ToUpper())
+    {
+        case "A":
+            op = " + ";
+            result = firstNumber + secondNumber;
+            break;
+        case "S":
+            op = " - ";
+            result = firstNumber - secondNumber;
+            break;
+        case "M":
+            op = " - ";
+            result = firstNumber - secondNumber;
+            break;
+    }
 
-if (op == "" && result == 0)
-{
-    Console.WriteLine("Invalid letter");
-} else
-{
-    Console.WriteLine(firstNumber + op + secondNumber + " = " + result);
+    if (op == "" && result == 0)
+    {
+        Console.WriteLine("Invalid letter");
+    }
+    else
+    {
+        Console.WriteLine(firstNumber + op + secondNumber + " = " + result);
+    }
 }
 
 Console.WriteLine("Press any key to close.");
